053197f4b601928e27bdae301999cac77aa58815
Lars Olesen <lars@intraface.dk>
2020-01-12 18:31:37 +0000

    Started to add tests

---
+++ src/test/compound.js
@@ -0,0 +1,10 @@
+const compound = require('../js/compound.js');
+var assert = require('assert');
+
+describe('Compound', function() {
+  describe('Compound', function() {
+    it('should return the correct number', function() {
+      assert.equal(1, 2);
+    });
+  });
+});
